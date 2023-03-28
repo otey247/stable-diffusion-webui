d0223c86fb62ac72b04b003c271c4c6af7025340
Jo Otey <otey247@gmail.com>
2023-03-28 18:19:16 -0500

    Updates

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
-@echo off
+ @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers --api --cors-allow-origins=http://127.0.0.1:3456
 
 call webui.bat
